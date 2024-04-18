681a5b5156bbef672a0113d9d95a48333bd82cae
unknown <martins.lagzdins@sk.lvt.lv>
2024-04-18 15:06:52 +0300

    Pabeigts picas objekts, bez īpašām funkcijām

--- src/Pizzeria/Pizza.java
+++ src/Pizzeria/Pizza.java
@@ -1,10 +1,11 @@
 package Pizzeria;
 
 public class Pizza {
-		String dough, sauce, topping;
-		Double diameter;
-		int amount;
-		boolean coupon;
+		private String dough, sauce, topping;
+		private Double diameter;
+		private int amount;
+		private boolean coupon;
+		
 		public Pizza(String dough, String sauce, String topping, Double diameter, int amount, boolean coupon) {
 			this.dough = dough;
 			this.sauce = sauce;
@@ -12,6 +13,45 @@ public class Pizza {
 			this.diameter = diameter;
 			this.amount = amount;
 			this.coupon = coupon;
-			
 	}
+		//setter
+		public void setDough(String newD) {
+			dough=newD;
+		}
+		public void setSauce(String newS) {
+			sauce=newS;
+		}
+		public void setTopping(String newT) {
+			topping=newT;
+		}
+		public void setDiameter(Double newD) {
+			diameter=newD;
+		}
+		public void setAmount(int newA) {
+			amount=newA;
+		}
+		public void setCoupon(boolean newC) {
+			coupon=newC;
+		}
+		
+		//getter
+		public String getDough() {
+			return dough;
+		}
+		public String getSauce() {
+			return sauce;
+		}
+		public String getTopping() {
+			return topping;
+		}
+		public Double getDiamater() {
+			return diameter;
+		}
+		public int getAmount() {
+			return amount;
+		}
+		public boolean isCoupon() {
+			return coupon;
+		}
+		
 }
