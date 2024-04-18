591537e6e13c682423006ab29218eb19f7167e13
unknown <martins.lagzdins@sk.lvt.lv>
2024-04-18 10:49:09 +0300

    Iesāku objektu

---
+++ src/Pizzeria/Pizza.java
@@ -0,0 +1,17 @@
+package Pizzeria;
+
+public class Pizza {
+		String dough, sauce, topping;
+		Double diameter;
+		int amount;
+		boolean coupon;
+		public Pizza(String dough, String sauce, String topping, Double diameter, int amount, boolean coupon) {
+			this.dough = dough;
+			this.sauce = sauce;
+			this.topping = topping;
+			this.diameter = diameter;
+			this.amount = amount;
+			this.coupon = coupon;
+			
+	}
+}
